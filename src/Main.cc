4f10d59bfe015b8520a60dc62c0b3f5ad79cc598
Nico <klaassen.nico@gmail.com>
2020-03-27 16:53:34 +0100

    Added pin layout

--- src/Main.cc
+++ src/Main.cc
@@ -5,9 +5,55 @@
 #include <iostream>
 #include "wiringPi.h"
 
-#define LED_PIN 0
-#define SENSOR_PIN 1
+/*******************************************************************************
+ * PIN LAYOUT
+ ******************************************************************************/
+// General outputs
+#define MOVE_BELT_OUT_PIN 7
+#define MAIN_PUSHER_OUT_PIN 0
+#define WHITE_PUSHER_OUT_PIN 2
+#define BLACK_PUSHER_OUT_PIN 3
 
+// General inputs
+#define MAIN_PRESENCE_IN_PIN 1
+#define MAIN_PUSHER_ENDSTOP_IN_PIN 6
+#define WHITE_SENSOR_IN_PIN 26
+#define BLACK_SENSOR_IN_PIN 27
+
+// Pins which encode information via combinations
+#define STATUS_2_OUT_PIN 21
+#define STATUS_1_OUT_PIN 22
+#define STATUS_0_OUT_PIN 23
+
+#define COLOR_SENSOR_0_IN_PIN 4
+#define COLOR_SENSOR_1_IN_PIN 5
+
+/*******************************************************************************
+ * Initial setup wiringPi
+ ******************************************************************************/
+wiringPiSetup(); // Regular wiringPi pin mode setup
+
+// OUTPUT PINS
+pinMode(MOVE_BELT_OUT_PIN, OUTPUT);
+pinMode(MAIN_PUSHER_OUT_PIN, OUTPUT);
+pinMode(WHITE_PUSHER_OUT_PIN, OUTPUT);
+pinMode(BLACK_PUSHER_OUT_PIN, OUTPUT);
+pinMode(STATUS_2_OUT_PIN, OUTPUT);
+pinMode(STATUS_1_OUT_PIN, OUTPUT);
+pinMode(STATUS_0_OUT_PIN, OUTPUT);
+
+// INPUT PINS
+pinMode(MAIN_PRESENCE_IN_PIN, INPUT);
+pinMode(MAIN_PUSHER_ENDSTOP_IN_PIN, INPUT);
+pinMode(WHITE_SENSOR_IN_PIN, INPUT);
+pinMode(BLACK_SENSOR_IN_PIN, INPUT);
+pinMode(COLOR_SENSOR_0_IN_PIN, INPUT);
+pinMode(COLOR_SENSOR_1_IN_PIN, INPUT);
+
+
+/*******************************************************************************
+ * ENTRY POINT CODE
+ ******************************************************************************/
 int main(int argc, char* argv[]) {
 	dzn::locator locator;
 	dzn::runtime runtime;
@@ -17,24 +63,11 @@ int main(int argc, char* argv[]) {
 
 	robbie_de_robot.check_bindings();
 
-	/**
-	 * Initial setup wiringPi
-	 */
-	wiringPiSetup();
-
-	// INPUT PINS
-	pinMode(LED_PIN, OUTPUT);
-
-	// OUTPUT PINS
-	pinMode(SENSOR_PIN, INPUT);
-
 	// Initialize pins
-	robbie_de_robot.led.led.in.initialise(LED_PIN);
-	robbie_de_robot.sensor.sensor.in.initialise(SENSOR_PIN);
+	robbie_de_robot.led.led.in.initialise(MAIN_PUSHER_OUT_PIN); // TODO: Use correct pin.
+	robbie_de_robot.sensor.sensor.in.initialise(MAIN_PRESENCE_IN_PIN); // TODO: Use correct pin.
 
-	/** 
-	 * Start the system! Just like Sten! <3
-	 */
+	// Start the system! Just like Sten! <3
 	robbie_de_robot.master.in.start();
 
 	while (true) {
