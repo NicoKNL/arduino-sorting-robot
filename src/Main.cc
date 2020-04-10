9ec8e60f14fb0cd1636aff8867d588deb733494c
Nico <klaassen.nico@gmail.com>
2020-04-10 11:53:48 +0200

    communication running in conjunction with dezyne

--- src/Main.cc
+++ src/Main.cc
@@ -1,12 +1,15 @@
 #include <dzn/runtime.hh>
 #include <dzn/locator.hh>
 
+#include "commutils.h"
+
 #include "SortingRobot.hh"
 #include <iostream>
 #include "wiringPi.h"
 #include <vector>
 #include <string>
 #include "config.hh"
+
 /*******************************************************************************
  * PIN LAYOUT
  ******************************************************************************/
@@ -32,104 +35,147 @@
 
 
 void logState(SortingRobotSystem robo, std::vector<std::string> translation) {
-	std::cout << "\n\n    [STATE] " << translation[robo.master.in.getState()] << "\n\n";
+    std::cout << "\n\n    [STATE] " << translation[robo.master.in.getState()] << "\n\n";
 }
 
 /*******************************************************************************
  * ENTRY POINT CODE
  ******************************************************************************/
 int main(int argc, char* argv[]) {
-	dzn::locator locator;
-	dzn::runtime runtime;
-	locator.set(runtime);
+    dzn::locator locator;
+    dzn::runtime runtime;
+    locator.set(runtime);
 
-	std::vector<std::string> t = // translation
-		{
-			"Off", "Idle", " Waiting", "Error", "IngestingDisk", "Sorting"
-		};
+    std::vector<std::string> t = // translation
+    {
+    "Off", "Idle", " Waiting", "Error", "IngestingDisk", "Sorting"
+    };
 
-	/*******************************************************************************
-	 * Initial setup wiringPi
-	 ******************************************************************************/
-	wiringPiSetup(); // Regular wiringPi pin mode setup
+    // Initialize libmosquitto
+    if (!setup_mqtt()) return 1;
 
-	// OUTPUT PINS
-	pinMode(MOVE_BELT_OUT_PIN, OUTPUT);
-	pinMode(MAIN_PUSHER_OUT_PIN, OUTPUT);
-	pinMode(WHITE_PUSHER_OUT_PIN, OUTPUT);
-	pinMode(BLACK_PUSHER_OUT_PIN, OUTPUT);
+    // Dezyne trigger variables
+    bool fatalError = false;
+    bool robotFailsFairness = false;
 
-	pinMode(STATUS_2_OUT_PIN, OUTPUT);
-	pinMode(STATUS_1_OUT_PIN, OUTPUT);
-	pinMode(STATUS_0_OUT_PIN, OUTPUT);
+    int disk_taking = 1;
 
-	// INPUT PINS
-	pinMode(MAIN_PRESENCE_IN_PIN, INPUT);
-	pinMode(MAIN_PUSHER_ENDSTOP_IN_PIN, INPUT);
-	pinMode(WHITE_SENSOR_IN_PIN, INPUT);
-	pinMode(BLACK_SENSOR_IN_PIN, INPUT);
-	pinMode(COLOR_SENSOR_0_IN_PIN, INPUT);
-	pinMode(COLOR_SENSOR_1_IN_PIN, INPUT);
+    /*******************************************************************************
+     * Initial setup wiringPi
+     ******************************************************************************/
+    wiringPiSetup(); // Regular wiringPi pin mode setup
 
-	// ENSURE ALL PINS TO LOW
-	digitalWrite(MOVE_BELT_OUT_PIN, LOW);
-	digitalWrite(MAIN_PUSHER_OUT_PIN, LOW);
-	digitalWrite(WHITE_PUSHER_OUT_PIN, LOW);
-	digitalWrite(BLACK_PUSHER_OUT_PIN, LOW);
-	digitalWrite(STATUS_2_OUT_PIN, LOW);
-	digitalWrite(STATUS_1_OUT_PIN, LOW);
-	digitalWrite(STATUS_0_OUT_PIN, LOW);
+    // OUTPUT PINS
+    pinMode(MOVE_BELT_OUT_PIN, OUTPUT);
+    pinMode(MAIN_PUSHER_OUT_PIN, OUTPUT);
+    pinMode(WHITE_PUSHER_OUT_PIN, OUTPUT);
+    pinMode(BLACK_PUSHER_OUT_PIN, OUTPUT);
 
-	SortingRobotSystem robbie_de_robot(locator);
+    pinMode(STATUS_2_OUT_PIN, OUTPUT);
+    pinMode(STATUS_1_OUT_PIN, OUTPUT);
+    pinMode(STATUS_0_OUT_PIN, OUTPUT);
 
-	robbie_de_robot.check_bindings();
+    // INPUT PINS
+    pinMode(MAIN_PRESENCE_IN_PIN, INPUT);
+    pinMode(MAIN_PUSHER_ENDSTOP_IN_PIN, INPUT);
+    pinMode(WHITE_SENSOR_IN_PIN, INPUT);
+    pinMode(BLACK_SENSOR_IN_PIN, INPUT);
+    pinMode(COLOR_SENSOR_0_IN_PIN, INPUT);
+    pinMode(COLOR_SENSOR_1_IN_PIN, INPUT);
 
-	// Initialize pins on components
-	robbie_de_robot.whiteActuator.actuator.in.initialise(WHITE_PUSHER_OUT_PIN);
-	robbie_de_robot.blackActuator.actuator.in.initialise(BLACK_PUSHER_OUT_PIN);
-	robbie_de_robot.wheelMotor.motor.in.initialise(MAIN_PUSHER_OUT_PIN);
-	robbie_de_robot.beltMotor.motor.in.initialise(MOVE_BELT_OUT_PIN);
+    // ENSURE ALL PINS TO LOW
+    digitalWrite(MOVE_BELT_OUT_PIN, LOW);
+    digitalWrite(MAIN_PUSHER_OUT_PIN, LOW);
+    digitalWrite(WHITE_PUSHER_OUT_PIN, LOW);
+    digitalWrite(BLACK_PUSHER_OUT_PIN, LOW);
+    digitalWrite(STATUS_2_OUT_PIN, LOW);
+    digitalWrite(STATUS_1_OUT_PIN, LOW);
+    digitalWrite(STATUS_0_OUT_PIN, LOW);
 
-	robbie_de_robot.factorFloorSensor.sensor.in.initialise(MAIN_PRESENCE_IN_PIN);
-	robbie_de_robot.wheelStopSensor.sensor.in.initialise(MAIN_PUSHER_ENDSTOP_IN_PIN);
-	robbie_de_robot.cs.colourSensor.in.initialise(COLOR_SENSOR_0_IN_PIN, COLOR_SENSOR_1_IN_PIN);
-	robbie_de_robot.beltSensorWhite.sensor.in.initialise(WHITE_SENSOR_IN_PIN);
-	robbie_de_robot.beltSensorBlack.sensor.in.initialise(BLACK_SENSOR_IN_PIN);
+    SortingRobotSystem robbie_de_robot(locator);
 
-	// Start the system! Just like Sten! <3
-	robbie_de_robot.master.in.start();
+    robbie_de_robot.check_bindings();
 
-	while (true) {
-		std::cout << "step\n";
-		logState(robbie_de_robot, t);
-		// Check timers
-		robbie_de_robot.ingestTimer.check_timer();
-		logState(robbie_de_robot, t);
-		robbie_de_robot.sortingTimer.check_timer();
-		logState(robbie_de_robot, t);
+    // Initialize pins on components
+    robbie_de_robot.whiteActuator.actuator.in.initialise(WHITE_PUSHER_OUT_PIN);
+    robbie_de_robot.blackActuator.actuator.in.initialise(BLACK_PUSHER_OUT_PIN);
+    robbie_de_robot.wheelMotor.motor.in.initialise(MAIN_PUSHER_OUT_PIN);
+    robbie_de_robot.beltMotor.motor.in.initialise(MOVE_BELT_OUT_PIN);
+
+    robbie_de_robot.factorFloorSensor.sensor.in.initialise(MAIN_PRESENCE_IN_PIN);
+    robbie_de_robot.wheelStopSensor.sensor.in.initialise(MAIN_PUSHER_ENDSTOP_IN_PIN);
+    robbie_de_robot.cs.colourSensor.in.initialise(COLOR_SENSOR_0_IN_PIN, COLOR_SENSOR_1_IN_PIN);
+    robbie_de_robot.beltSensorWhite.sensor.in.initialise(WHITE_SENSOR_IN_PIN);
+    robbie_de_robot.beltSensorBlack.sensor.in.initialise(BLACK_SENSOR_IN_PIN);
+
+    // Start the system! Just like Sten! <3
+    robbie_de_robot.master.in.start();
+
+    while (true) {
+        // if (debug) {
+        std::cout << "step\n";
+
+        heartbeat();
+
+        // TODO:
+            // When we take disk, increment counter!
+
+        //TODO: after taking a disk, publish the message
+        if (disk_taking % 15 == 0) {
+            take_disk();
+        }
+        disk_taking++;
+        // TODO: what logic triggers fatalError = true? Determine what kind of
+        // error would this be
+        if (fatalError == true) {
+            raise_emergency_stop();
+        }
+
+        // TODO: what logic triggers this? After detecting that our robot takes
+        // "too many" or "too few" disks compared to the others
+        if (robotFailsFairness == true) {
+            raise_error();
+        }
 
 
-		// Input sensor detection
-		robbie_de_robot.factorFloorSensor.detect();
-		logState(robbie_de_robot, t);
 
-		robbie_de_robot.wheelStopSensor.detect();
-		logState(robbie_de_robot, t);
 
-		robbie_de_robot.cs.detect();
-		logState(robbie_de_robot, t);
+        logState(robbie_de_robot, t);
+        // Check timers
+        robbie_de_robot.ingestTimer.check_timer();
+        logState(robbie_de_robot, t);
+        robbie_de_robot.sortingTimer.check_timer();
+        logState(robbie_de_robot, t);
 
-		robbie_de_robot.beltSensorWhite.detect();
-		logState(robbie_de_robot, t);
 
-		robbie_de_robot.beltSensorBlack.detect();
-		logState(robbie_de_robot, t);
+        // Input sensor detection
+        robbie_de_robot.factorFloorSensor.detect();
+        logState(robbie_de_robot, t);
 
-		std::cout << "post step\n";
+        robbie_de_robot.wheelStopSensor.detect();
+        logState(robbie_de_robot, t);
 
-		if (Debug::DEBUG) {
-			delay(500); // 500 ms
-		}
-	}
-	return 0;
+        robbie_de_robot.cs.detect();
+        logState(robbie_de_robot, t);
+
+        robbie_de_robot.beltSensorWhite.detect();
+        logState(robbie_de_robot, t);
+
+        robbie_de_robot.beltSensorBlack.detect();
+        logState(robbie_de_robot, t);
+
+        std::cout << "post step\n";
+
+        // Finally, update the external hearbeat counts to track how long we
+        // haven't heard from other bots on the factory floor
+        update_external_hearbeats();
+
+        delay(1000); // 1000 ms
+    }
+
+    // Exiting program. Cleanup.
+    destroy_mqtt();
+    // TODO: wiringPi cleanup...
+
+    return 0;
 }
--- src/commutils.h
+++ src/commutils.h
@@ -9,8 +9,8 @@
 #include <string>
 #include <sstream>
 
-const char * BROKER_ADDRESS = "86.91.204.180";
-// const char * BROKER_ADDRESS = "127.0.0.1";
+// const char * BROKER_ADDRESS = "86.91.204.180";
+const char * BROKER_ADDRESS = "127.0.0.1";
 const int MQTT_PORT = 1883;
 // const int MQTT_PORT = 23321;
 
@@ -19,7 +19,7 @@ const char * MQTT_TOPIC_OUT = "factory/robot2/out";
 const int OUR_ROBOT_ID = 2;
 const int KEEPALIVE = 60;
 const int HEARTBEAT_DELAY = 5;
-const int MAX_HEARTBEAT_DELAY = 60; // TODO: Change back to 60
+const int MAX_HEARTBEAT_DELAY = 5; // TODO: Change back to 60
 
 int HEARTBEAT_TRACKER = 0;
 std::vector<bool> EXTERNAL_ALIVE = {0, 1, 1, 1, 1};
